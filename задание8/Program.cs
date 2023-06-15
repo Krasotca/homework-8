0566e61b4eec9c1328cc3ea578bbdf4ecc863786
Оstrovaya Еlena <elena.ostrowaya@yandex.ru>
2023-06-16 02:11:49 +0300

    задание8

---
+++ задание8/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите число:");
+string userInput1 = Console.ReadLine() ?? "";
+int n = int.Parse(userInput1);
+
+int count = 1;
+while (count <= n)
+{
+    int what = new Random().Next(1, n);
+    if (what % 2 == 0)
+    {
+        Console.WriteLine($" what = {what}");
+        count = count + 1;
+    }
+}
+
